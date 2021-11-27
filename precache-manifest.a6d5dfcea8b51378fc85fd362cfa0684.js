31585a4379093f353fe4e780a1fc5e6f58da4498
solismatias <solisma42@gmail.com>
2021-11-27 02:02:23 -0300

    Updates

---
+++ precache-manifest.a6d5dfcea8b51378fc85fd362cfa0684.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363f7ef9f1ae544e7fd0f8c3f407e824",
+    "url": "/leaguemaker/index.html"
+  },
+  {
+    "revision": "23c163da43272b6097c9",
+    "url": "/leaguemaker/static/css/main.be5394a8.chunk.css"
+  },
+  {
+    "revision": "1eae5334a59456bc75e8",
+    "url": "/leaguemaker/static/js/2.4e5c67e6.chunk.js"
+  },
+  {
+    "revision": "2e7a0b14f6d1081167a5a052c8545f6d",
+    "url": "/leaguemaker/static/js/2.4e5c67e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c163da43272b6097c9",
+    "url": "/leaguemaker/static/js/main.6609b8b1.chunk.js"
+  },
+  {
+    "revision": "e73994b87f37d6c0bd35",
+    "url": "/leaguemaker/static/js/runtime-main.fc9bc53d.js"
+  },
+  {
+    "revision": "09e1553ef1ec4b372a50bccb51e9aca2",
+    "url": "/leaguemaker/static/media/bat.09e1553e.svg"
+  },
+  {
+    "revision": "3a65ca8573ffa2b934e389e558e2e076",
+    "url": "/leaguemaker/static/media/check.3a65ca85.svg"
+  },
+  {
+    "revision": "5f761130ea98e8e443aa89dcc4a25b5f",
+    "url": "/leaguemaker/static/media/logo.5f761130.svg"
+  },
+  {
+    "revision": "178488a9cec683149dd5c537b572ba4c",
+    "url": "/leaguemaker/static/media/logotxt.178488a9.svg"
+  },
+  {
+    "revision": "dbbe494b833412d3852876e83e3e8dd7",
+    "url": "/leaguemaker/static/media/stat.dbbe494b.svg"
+  }
+]);
